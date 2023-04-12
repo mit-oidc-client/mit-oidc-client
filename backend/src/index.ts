147e47bd166b2bc59b859ae943ff1293a99bc4ac
Huy Dai <huydai@mit.edu>
2023-04-11 23:23:24 -0400

    Add support for HTTPS in Express backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,29 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 
+// Load in environment variables
 dotenv.config();
+
+const https = require("https");
+const fs = require("fs");
 
 const app: Express = express();
 const port = process.env.PORT;
 
+// start the Express server
+const server = https
+    .createServer(
+    {
+        key: fs.readFileSync(process.env.SSL_KEY_FILE),
+        cert: fs.readFileSync(process.env.SSL_CRT_FILE),
+    },
+    app
+    )
+    .listen(port, () => {
+        console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+    });
 
 // define a route handler for the default home page
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
-});
-
-// start the Express server
-app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
